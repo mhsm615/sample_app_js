26f47af372d5f54586c900ff4103cc8a306251ae
mhsm615 <id.fk.h2@icloud.com>
2025-05-22 18:03:06 +0900

    seedデータ作成

--- fruits-backend/db/seeds.rb
+++ fruits-backend/db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Fruit.create!(name: "りんご", category: "赤")
+Fruit.create!(name: "バナナ", category: "黄色")
+Fruit.create!(name: "ぶどう", category: "紫")
