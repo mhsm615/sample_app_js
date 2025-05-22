773b9ef089ff34d22aa29b237ebb32cd8a4042e6
mhsm615 <id.fk.h2@icloud.com>
2025-05-22 20:21:14 +0900

    フロントエンドをtypescriptに変更し、rails  apiモードで起動するためviteを導入

---
+++ fruits-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import FruitList from "./compornents/FruitList.tsx";
+
+function App() {
+  return (
+    <div>
+      <h1>フルーツ一覧</h1>
+      <FruitList />
+    </div>
+  );
+}
+
+export default App
--- fruits-frontend/src/compornents/App.js
+++ fruits-frontend/src/compornents/App.js
@@ -1,7 +1,7 @@
 import "./styles.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 
 export default function App() {
   return (
